4af0b0b68704bb5c8189d5e5666868d1bc3d253e
Dazaa1 <marouanelankaoui066@gmail.com>
2025-06-07 08:11:48 +0100

    Finish more styling.

--- js/main.js
+++ js/main.js
@@ -4,15 +4,25 @@ const browserVersion = document.createElement("h2");
 const body = document.querySelector("#body")
 
 
+
+
 const getUserAgent = () => {
     // parse user agent
     let result = bowser.getParser(navigator.userAgent).getResult();
     
-    browserName.textContent = "Browser name: " + result.browser.name;
-    browserVersion.textContent = "Browser version: " + result.browser.version;
+    browserName.textContent = "Browser Name: " + result.browser.name;
+    browserVersion.textContent = "Browser Version: " + result.browser.version;
     body.append(browserName);
     body.append(browserVersion);
 }
 
 // adding an event listener listening to load event.
 window.addEventListener("load", getUserAgent());
+
+body.style.display = "flex";
+body.style.flexDirection = "column";
+browserName.style.marginLeft = "auto";
+browserName.style.marginRight = "auto";
+browserVersion.style.marginLeft = "auto";
+browserVersion.style.marginRight = "auto";
+browserName.style.marginTop = "200px";
